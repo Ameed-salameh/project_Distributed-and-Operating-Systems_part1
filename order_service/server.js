4c844a748ed4b88cd0b3ba6762d0bb76f642c56b
Ameed-salameh <ameed.salame123@gmail.com>
2025-10-27 00:43:29 +0200

    added catalog_service and order_service

---
+++ order_service/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.get('/', (req, res) => {
+  res.send('order_service is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`order_service listening on port ${PORT}`);
+});
